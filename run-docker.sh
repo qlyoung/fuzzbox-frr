cdb0064fbbba1b316fbd7e14a6f403b6d2e7e181
Quentin Young <qlyoung@cumulusnetworks.com>
2020-01-06 20:52:57 -0500

    update ref to qlyoung/fuzzbox-frr

--- run-docker.sh
+++ run-docker.sh
@@ -21,4 +21,5 @@ printf "Results are saved in the container filesystem.\n"
 printf "Container /opt filesystem details: %s\n" $VOLDETAILS
 sleep 1
 
-docker run -it -p 3000:3000 -p 8086:8086 --mount source=$VOL,target=/opt `docker images -q | head -n 1` $PROTO $CORES
+#docker run -it -p 3000:3000 -p 8086:8086 --mount source=$VOL,target=/opt `docker images -q | head -n 1` $PROTO $CORES
+docker run -it -p 3000:3000 -p 8086:8086 --mount source=$VOL,target=/opt qlyoung/fuzzbox-frr $PROTO $CORES
